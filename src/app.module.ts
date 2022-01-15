cb3ea661913b77b1a4b3a22ba04c05554f89d0cb
anasMtukur <anasmtukur91@gmail.com>
2022-01-15 16:59:38 +0800

    Fix mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,25 @@ import { UserModule } from './user/user.module';
 import { CompanyModule } from './company/company.module';
 import { UserService } from './user/user.service';
 import { CompanyService } from './company/company.service';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { NotificationTypeModule } from './notification-type/notification-type.module';
+import { NotificationTypeService } from './notification-type/notification-type.service';
+import { NotificationModule } from './notification/notification.module';
 
+const url = process.env.MONGO_URL || 'localhost';
+const username = process.env.MONGO_USERNAME || 'root';
+const password = process.env.MONGO_PASSWORD || 'pass12345';
 @Module({
-  imports: [UserModule, CompanyModule],
+  imports: [
+    UserModule,
+    CompanyModule,
+    TypegooseModule.forRoot(
+      `mongodb://${username}:${password}@${url}:27017?serverSelectionTimeoutMS=2000&authSource=admin`,
+    ),
+    NotificationTypeModule,
+    NotificationModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, UserService, CompanyService],
+  providers: [AppService, UserService, CompanyService, NotificationTypeService],
 })
 export class AppModule {}
